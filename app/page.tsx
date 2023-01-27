635969f9e6d613e13e235a1bc9e98338c1747ecb
Alberto <alberto@mondey.co>
2023-01-27 17:06:47 +0100

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,63 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+'use client';
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
+import { useMemo, useState } from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | null>(null);
+
+  function startTimer() {
+    if (!isRunning) {
+      setIsRunning(true);
+      setIntervalId(
+        setInterval(() => {
+          setTime((time) => time + 1);
+        }, 1000)
+      );
+    }
+  }
+
+  function stopTimer() {
+    if (isRunning && intervalId !== null) {
+      setIsRunning(false);
+      clearInterval(intervalId);
+    }
+  }
+
+  function resetTimer() {
+    stopTimer();
+    setTime(0);
+  }
+
+  const formattedTime = useMemo(() => {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    return { hours: `${hours < 10 ? '0' + hours : hours}`, minutes: `${minutes < 10 ? '0' + minutes : minutes}`, seconds: `${seconds < 10 ? '0' + seconds : seconds}` };
+  }, [time]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className={`${inter.className} ${styles.main}`}>
+      <h1>
+        {formattedTime.hours}:{formattedTime.minutes}:{formattedTime.seconds}
+      </h1>
+      <div className={styles.buttons}>
+        <button type='button' onClick={startTimer}>
+          Start
+        </button>
+        <button type='button' onClick={stopTimer}>
+          Stop
+        </button>
+        <button type='button' onClick={resetTimer}>
+          Reset
+        </button>
       </div>
     </main>
-  )
+  );
 }

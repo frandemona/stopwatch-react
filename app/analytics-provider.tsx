eee91fa74234c08c6f61fc5de5b4f20983d52218
Alberto <alberto@mondey.co>
2023-01-29 20:05:51 +0100

    Analytics adaptation

--- app/analytics-provider.tsx
+++ app/analytics-provider.tsx
@@ -1,30 +1,35 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { PropsWithChildren, useEffect } from 'react';
 import * as gtag from '../lib/gtag';
 // import { pageview } from '../lib/gtm';
 
 export default function AnalyticsProvider({ children }: PropsWithChildren) {
-  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
       gtag.pageview(url);
     };
+
+    const url = pathname + searchParams.toString();
+
     // GTAG
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
+    // router.events.on('routeChangeComplete', handleRouteChange);
+    // router.events.on('hashChangeComplete', handleRouteChange);
+    handleRouteChange(url);
     // GTM
     // router.events.on('routeChangeComplete', pageview);
     return () => {
       // GTAG
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
+      // router.events.off('routeChangeComplete', handleRouteChange);
+      // router.events.off('hashChangeComplete', handleRouteChange);
       // GTM
       // router.events.off('routeChangeComplete', pageview);
     };
-  }, [router.events]);
+  }, [pathname, searchParams]);
 
   return <>{children}</>;
 }
